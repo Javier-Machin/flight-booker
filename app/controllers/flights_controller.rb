362962681ca2b4100b76c68d985687cdc3ccfe89
Javier Machin <javier_machin_acosta@hotmail.com>
2018-05-30 16:17:34 +0100

    Add flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    unless params[:search].nil?
+      @search_result = Flight.where(origin: params[:search][:origin], 
+                                    destination: params[:search][:destination])
+    end
   end
 end
