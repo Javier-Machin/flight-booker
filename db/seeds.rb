3845624ddd80fcfa08310c96191961dc7f4d63cf
Javier Machin <javier_machin_acosta@hotmail.com>
2018-05-30 14:25:08 +0100

    Add flights seeds, controller,routes and index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Airports
+
+Airport.create(name: "NYC")
+Airport.create(name: "LA")
+Airport.create(name: "Madrid")
+Airport.create(name: "Barcelona")
+Airport.create(name: "Glasgow")
+Airport.create(name: "Stuttgart")
+
+# Create Fligths
+
+Flight.create(origin: Airport.first, destination: Airport.second, 
+              departure: "2020-05-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.first, destination: Airport.second, 
+              departure: "2020-06-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.first, destination: Airport.second, 
+              departure: "2020-07-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.third, destination: Airport.fourth, 
+              departure: "2020-05-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.third, destination: Airport.fourth, 
+              departure: "2020-06-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.third, destination: Airport.fourth, 
+              departure: "2020-07-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.find(5), destination: Airport.find(6), 
+              departure: "2020-05-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.find(5), destination: Airport.find(6), 
+              departure: "2020-06-25 16:00", duration: 300)
+
+Flight.create(origin: Airport.find(5), destination: Airport.find(6), 
+              departure: "2020-07-25 16:00", duration: 300)
