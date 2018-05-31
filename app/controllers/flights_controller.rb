93594dabd2839ed394a355831fdf378042a82d8c
Javier Machin <javier_machin_acosta@hotmail.com>
2018-05-31 12:44:42 +0100

    Add available fligths form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,7 @@ class FlightsController < ApplicationController
     unless params[:search].nil?
       @search_result = Flight.where(origin: params[:search][:origin], 
                                     destination: params[:search][:destination])
+      @passengers_num = params[:search][:passengers_num]
     end
   end
 end
