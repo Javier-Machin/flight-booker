cc86d8d941575d9916e7d2b5f27b8a99fde2c897
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-01 18:53:00 +0100

    Add booking create action and booking index view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,24 @@ class BookingsController < ApplicationController
     @passengers_num = params[:flight_selection][:passengers_num]
   end
 
+  def index
+    @bookings = Booking.all
+  end 
+
   def create
+    @passengers_num = params[:booking][:passengers_num].to_i
+    
+    @passengers_num.times do |index|
+      @booking = Booking.new(flight: Flight.find(params[:booking][:flight]),
+                             passenger_attributes: {name: params[:passengers][:"name#{index+1}"],
+                                                    email: params[:passengers][:"email#{index+1}"]})
+
+      # @booking.passenger_attributes = {name: params[:passengers][:"name#{index+1}"],
+      #                                  email: params[:passengers][:"email#{index+1}"]}
+      
+      @booking.save
+    end
+
+    redirect_to bookings_path
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :passenger
   belongs_to :flight
+  accepts_nested_attributes_for :passenger
 end
