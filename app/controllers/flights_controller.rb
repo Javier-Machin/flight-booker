c90a3f4cc4b74984812c7f16686bbd774ff30463
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-02 17:53:43 +0100

    Add search form remember selection functionality, rename CSS classes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,7 @@ class FlightsController < ApplicationController
     unless params[:search].nil?
       @search_result = Flight.where(origin: params[:search][:origin], 
                                     destination: params[:search][:destination],
-                                    departure: DateTime.parse(params[:search][:date]))
+                                    departure: DateTime.parse(params[:search][:date])).all
       @passengers_num = params[:search][:passenger_num]
     end
   end
